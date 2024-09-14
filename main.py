5ddba4109c83208a2c90756d4f028706c189598b
A1ekseyN <lolik4ever@rambler.ru>
2024-09-14 13:45:15 +0300

    Version: 0.0.4a - Add item file with ships - Add item file with modules - Added an infinite loop that monitors ships and modules

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 # pyinstaller -F --icon=icon_parser.ico main.py
+# pyinstaller -F --icon=icon_parser.ico main.py --hidden-import=pkg_resources
+
 import requests
 import json
 import time
+from tqdm import tqdm
 
 from items_all_market import items_eve
 from items_manual import items_sorted_manual
+from items_ships import items_ships
+from items_modules import items_modules
+
 
 # https://www.adam4eve.eu/info_locations.php
 
-version = "0.0.1a"
+version = "0.0.1b"
 start_time = time.time()
 
 items = items_eve                           # items_sorted_manual         # Список items, которые анализируются из рынка
+items = items_ships                         # Переменная со списком кораблей
+items = items_modules                       # Переменная с дорогими модулями
+items = items_ships + items_modules
+
 dump_orders_all = []
 filter_price_min = 200000                   # Фильтр минимальной цены
 #filter_price_min = 10000000                   # Фильтр минимальной цены
@@ -25,8 +35,6 @@ print(f'Items Count: {len(items)}')
 excluded_keywords = ['day', 'Days', 'Serenity', 'background', 'Non-interactable', 'non-interactable']
 items = [item for item in items if not any(keyword in item['name'] for keyword in excluded_keywords)]
 print(f'Filtered Items Count: {len(items)}')
-
-#items.reverse()                                   # Сортировка в обратную сторону
 
 
 def get_item_prices(item_id, item_name, region_id, region_name, station_id, station_name, sales_tax):
@@ -86,60 +94,50 @@ stations = [
 #    {"id": 60008494, "name": "Amarr VIII (Oris) - Emperor Family Academy"}
 ]
 
-for item in items:
-    item_id = item["id"]
-    item_name = item["name"]
+# Анализируем товары, прогресс будет отслеживаться только по товарам
+while True:
+#    for item in items:
+    for item in tqdm(items, desc=f"Analyzing items {regions[0]['name']}", colour="green"):
+        item_id = item["id"]
+        item_name = item["name"]
 
-    for region in regions:
+        # Проходим по каждому региону (в данном случае выбран только "The Forge")
+        region = regions[0]  # Заранее выбираем нужный регион (например, Jita)
         region_id = region["id"]
         region_name = region["name"]
 
-        for station in stations:
-            station_id = station["id"]
-            station_name = station["name"]
+        # Проходим по каждой станции (например, Jita 4-4)
+        station = stations[0]  # Заранее выбираем нужную станцию (например, Jita 4-4)
+        station_id = station["id"]
+        station_name = station["name"]
 
-            item_prices = get_item_prices(item_id, item_name, region_id, region_name, station_id, station_name, sales_tax)
+        # Получаем цены на товар
+        item_prices = get_item_prices(item_id, item_name, region_id, region_name, station_id, station_name, sales_tax)
 
-            if item_prices and station_id in item_prices:
-#                print(f"\nТовар: '{item_name}' в регионе '{region_name}' на станции '{station_name}':")
-                for station_id, station_prices in item_prices.items():
-                    # Поиск ордеров, при выставлении которых допущена ошибка (<= 0.1)
-                    if station_prices and station_prices["profit_sell_percentage"] <= 0.1 and station_prices["min_sell_price"] >= filter_price_min:
-                        print(f"\nТовар [id: {item['id']}]: '{item_name}' в регионе '{region_name}' на станции '{station_name}':")
-#                        print('.', end='')
-#                        print(f'id: {item["id"]} - OK ')
-                        min_sell_price = station_prices["min_sell_price"]
-                        max_buy_price = station_prices["max_buy_price"]
-                        trade_volume = station_prices["trade_volume"]
+        # Если есть данные по ценам и они соответствуют фильтрам
+        if item_prices and station_id in item_prices:
+            station_prices = item_prices[station_id]
+            if station_prices["profit_sell_percentage"] <= 0.1 and station_prices["min_sell_price"] >= filter_price_min:
+                print(f"\nТовар [id: {item['id']}]: '{item_name}' в регионе '{region_name}' на станции '{station_name}':")
+                min_sell_price = station_prices["min_sell_price"]
+                max_buy_price = station_prices["max_buy_price"]
+                trade_volume = station_prices["trade_volume"]
 
-                        profit_sell = station_prices["profit_sell"]
-                        profit_sell_percentage = station_prices["profit_sell_percentage"]
+                profit_sell = station_prices["profit_sell"]
+                profit_sell_percentage = station_prices["profit_sell_percentage"]
 
-                        print(f"Sell Price: {min_sell_price:,.0f}, Buy Price: {max_buy_price:,.0f}")
-                        print(f"Volume: - {trade_volume:,.0f} items")                # !!!
-                        print(f"Profit: {profit_sell:,.2f} isk - {profit_sell_percentage:.2f}%")
-#                    else:
-#                        print(f"- !!! Товар [id: {item['id']}]: '{item_name}'")
+                print(f"Sell Price: {min_sell_price:,.0f}, Buy Price: {max_buy_price:,.0f}")
+    #            print(f"Volume: - {trade_volume:,.0f} items")
+    #            print(f"Profit: {profit_sell:,.2f} isk - {profit_sell_percentage:.2f}%")
+                print()
 
-#                        # Проверка товаров
-#                        print(f"- Товар [id: {item['id']}]: '{item_name}' в регионе '{region_name}' на станции '{station_name}':")
+        if station_prices:
+            dump_orders_all.append(item_prices)
 
 
-#                    else:
-#                        pass
-#                        print(f"Цены недоступны для станции '{station_name}'")
-##                        print('.', end="")
-                    if station_prices:
-                        # Dump результатов
-                        dump_orders_all.append(item_prices)
-            else:
-                pass
-#                print(f"Не удалось получить цены на товар '{item_name}' в регионе '{region_name}' на станции '{station_name}'")
-
-
-with open("output.txt", "w", encoding="utf-8") as file:
-    json.dump(dump_orders_all, file)
-    print("File saved output.txt")
+#with open("output.txt", "w", encoding="utf-8") as file:
+#    json.dump(dump_orders_all, file)
+#    print("File saved output.txt")
 
 
 print(f'\nTime: {time.time() - start_time:,.2f} sec')
